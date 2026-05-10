fa2d184463a313421118a18a1d019e7ff683df0b
Amulya R <amulyar122006@gmail.com>
2026-05-10 17:55:16 +0530

    Update Inter_Thread_Communication.java

--- Inter_Thread_Communication.java
+++ Inter_Thread_Communication.java
@@ -1,45 +1,55 @@
 class Bank {
- int balance = 0;
-synchronized void deposit(int amount) {
-balance = balance + amount;
-System.out.println("Deposited: " + amount);
- notify();
-}
-synchronized void withdraw(int amount) {
- System.out.println("Trying to Withdraw");
- while(balance < amount) {
- System.out.println("Insufficient Balance");
-try {wait();
-} catch(Exception e) {
-  }
- }
- balance = balance - amount;
-  System.out.println( "Withdrawn: " + amount);
+    int balance = 0;
+
+    synchronized void deposit(int amount) {
+        balance = balance + amount;
+        System.out.println("Deposited: " + amount);
+        notify(); // Wakes up the waiting withdraw thread
+    }
+
+    synchronized void withdraw(int amount) {
+        System.out.println("Trying to Withdraw...");
+        while (balance < amount) {
+            System.out.println("Insufficient Balance, waiting for deposit...");
+            try {
+                wait(); // Releases the lock and waits
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        balance = balance - amount;
+        System.out.println("Withdrawn: " + amount + " | Remaining Balance: " + balance);
     }
 }
+
 class DepositThread extends Thread {
- Bank b;
-DepositThread(Bank b) {
- this.b = b;
+    Bank b;
+    DepositThread(Bank b) {
+        this.b = b;
     }
- public void run() {
-   b.deposit(5000);
+    public void run() {
+        try { Thread.sleep(1000); } catch (InterruptedException e) {}
+        b.deposit(5000);
     }
 }
+
 class WithdrawThread extends Thread {
- Bank b;
- WithdrawThread(Bank b) {
- } this.b = b;
-public void run() {
- b.withdraw(3000);
+    Bank b;
+    WithdrawThread(Bank b) {
+        this.b = b; 
+    }
+    public void run() {
+        b.withdraw(3000);
     }
 }
+
 public class Main {
-public static void main(String[] args) {
-Bank b = new Bank();
-DepositThread d =new DepositThread(b);
-WithdrawThread w =new WithdrawThread(b);
-w.start();
-d.start();
-}
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        DepositThread d = new DepositThread(b);
+        WithdrawThread w = new WithdrawThread(b);
+
+        w.start(); 
+        d.start(); 
+    }
 }
